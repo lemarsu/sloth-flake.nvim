eb11cfbe1a3a89d227855d6f2ace694058ab889f
LeMarsu <ch.ruelle@lemarsu.com>
2024-06-04 00:48:37 +0200

    fix: Sloth list was broken since refactoring

--- lua/sloth-flake/command/list.lua
+++ lua/sloth-flake/command/list.lua
@@ -4,17 +4,17 @@ local utils = require 'sloth-flake.utils'
 return function(args)
   local filter = args[1] or "all"
   local deps = vim.iter(Dep.all()):map(function(_, dep)
-    return dep:name()
+    return dep.name
   end)
   if filter == "all" then
     -- Nothing to do
   elseif filter == "loaded" then
     deps = deps:filter(function(dep)
-      return Dep.get(dep):is_loaded()
+      return Dep.get(dep).is_loaded
     end)
   elseif filter == "notloaded" then
     deps = deps:filter(function(dep)
-      return not Dep.get(dep):is_loaded()
+      return not Dep.get(dep).is_loaded
     end)
   else
     utils.error([[No Sloth list filter "%s".]], cmd)
