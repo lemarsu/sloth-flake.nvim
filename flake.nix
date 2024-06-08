cbd08e5258d7c5ae833ea7f7287c736689ac5fc3
LeMarsu <ch.ruelle@lemarsu.com>
2024-06-08 20:03:23 +0200

    chore: move back to flake-utils

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.4.0";
+    utils.url = "github:numtide/flake-utils";
     nil.url = "github:oxalica/nil";
     yants.url = "github:divnix/yants";
     alejandra = {
@@ -20,24 +20,25 @@
     yants,
     ...
   } @ inputs: let
-    versionFile = builtins.replaceStrings ["\n"] [""] (builtins.readFile ./VERSION);
+    inherit (builtins) readFile replaceStrings;
+    fu = utils.lib;
+    versionFile = replaceStrings ["\n"] [""] (readFile ./VERSION);
+    version =
+      if self.sourceInfo ? dirtyShortRev
+      then "${versionFile}-${self.sourceInfo.dirtyShortRev}"
+      else versionFile;
 
-    version = if self.sourceInfo ? dirtyShortRev
-    then "${versionFile}-${self.sourceInfo.dirtyShortRev}"
-    else versionFile;
-  in
-    utils.lib.mkFlake {
-      inherit self inputs;
-      outputsBuilder = channel: let
-        system = channel.nixpkgs.system;
-      in {
-        formatter = alejandra.defaultPackage.${channel.nixpkgs.system};
-        devShells.default = import ./shell.nix {
-          pkgs = channel.nixpkgs;
-          inherit (inputs.nil.packages.${system}) nil;
-        };
+    forSystem = system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      formatter = alejandra.defaultPackage.${system};
+      inherit (inputs.nil.packages.${system}) nil;
+    in {
+      inherit formatter;
+      devShells.default = import ./shell.nix {
+        inherit pkgs nil;
       };
-
-      lib = import ./lib {inherit version yants;};
     };
+  in
+    (fu.eachDefaultSystem forSystem)
+    // {lib = import ./lib {inherit version yants;};};
 }
